6390da6e49d883ed15a94ba72a3cd06ccb90b3af
Sayreks <v.bezgubovv1@gmail.com>
2023-12-14 14:50:11 +0500

    DZ_5

--- src/keyboard.py
+++ src/keyboard.py
@@ -16,8 +16,7 @@ class Mixin:
 
 
 class Keyboard(Item, Mixin):
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        super().__init__(name, price, quantity)
+    pass
 
     #
     # @language.setter
